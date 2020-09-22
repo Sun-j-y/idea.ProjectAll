8ce4ff1a63cd2e90658d8f4ac9caf38464c31901
SUN-JY <forits@qq.com>
2020-09-22 13:16:48 +0800

    first commit

---
+++ BaseTest/src/test/java/com/sforit/sbtest/SbTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sforit.sbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ mavenTest/src/main/java/testMain.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * @Author：sforits
+ * @E-mail：sforits@gmail.com
+ * @Date：2020/9/21-13:21 Created by IntelliJ IDEA.
+ */
+public class testMain {
+    @Test
+    public void t1(){
+
+    }
+}
